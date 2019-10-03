b30cfe5c32caa066634ac2792a873b5cbd67ee5d
Thomas Liske <thomas@fiasko-nw.net>
2019-10-03 15:34:20 +0200

    Shorten datetime imports names.

--- src/settings.ex/schedules.py
+++ src/settings.ex/schedules.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime as dt
+from datetime import timedelta as td
 import pytz
 
 SCHEDULES = [
@@ -17,15 +18,16 @@ SCHEDULES = [
         },
 
         # more card details
+        # https://wekan.github.io/api/v3.00/#put_board_list_card
         'details': {
             'description': 'first-come, first-served',
             'color': 'pink',
             'labelIds': ['<label-id>'],
             'members': ['<user-id>'],
             # set due time to 17 o'clock
-            #'dueAt': lambda: datetime.datetime.now().replace(hour=17, minute=0, second=0, microsecond=0).astimezone(pytz.utc).isoformat(),
+            # 'dueAt': lambda: dt.now().replace(hour=17, minute=0, second=0, microsecond=0).astimezone(pytz.utc).isoformat(),
             # set due time to tomorrow 17 o'clock
-            #'dueAt': lambda: (datetime.datetime.now() + datetime.timedelta(days=1)).replace(hour=17, minute=0, second=0, microsecond=0).astimezone(pytz.utc).isoformat(),
+            # 'dueAt': lambda: (dt.now() + td(days=1)).replace(hour=17, minute=0, second=0, microsecond=0).astimezone(pytz.utc).isoformat(),
         },
     },
 ]
